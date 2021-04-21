69cb7b0d737b47427322c9611625d03e227768b0
Ashish Arora <ashish007722@gmail.com>
2021-04-21 16:10:27 +0530

    User Repository update added methods to easily query database for user related functions
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- src/database/repository/UserRepo.ts
+++ src/database/repository/UserRepo.ts
@@ -6,4 +6,16 @@ export default class UserRepo extends Repository<User> {
 	findByEmail(email: string): Promise<User | undefined> {
 		return this.findOne({ email: email });
 	}
+
+	findByUuid(uuid: string): Promise<User | undefined> {
+		return this.findOne({ uuid: uuid });
+	}
+
+	findById(id: number): Promise<User | undefined> {
+		return this.findOne({ id: id });
+	}
+
+	async updateUser(user: User): Promise<void> {
+		await this.update({ id: user.id }, user);
+	}
 }
