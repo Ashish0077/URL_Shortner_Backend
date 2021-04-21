3be82ea5af1fae4ed82921dc5f0272f8aca1cfc8
Ashish Arora <ashish007722@gmail.com>
2021-04-21 17:13:53 +0530

    Links are private now create, delete, read operation on link is now only for authenticated users
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- src/controllers/v1/link/link.ts
+++ src/controllers/v1/link/link.ts
@@ -1,34 +1,36 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { getCustomRepository } from "typeorm";
 import crypto from "crypto";
 import LinkRepo from "../../../database/repository/LinkRepo";
 import asyncHandler from "../../../utils/asyncHandler";
 import { BadRequestError, NoEntryError } from "../../../core/ApiError";
 import { SuccessMsgResponse, SuccessResponse } from "../../../core/ApiResponse";
+import { ProtectedRequest } from "app-request";
 
-export const getLink = asyncHandler(async (req: Request, res: Response) => {
+export const getLink = asyncHandler(async (req: ProtectedRequest, res: Response) => {
 	const link = await getCustomRepository(LinkRepo).findByUuid(req.params.uuid);
 	if (!link) throw new NoEntryError("Link does not exist.");
 	link.shortUrl = `${req.protocol}://${req.get("host")}/${link.shortUrl}`;
 	new SuccessResponse("success", link).send(res);
 });
 
-export const createLink = asyncHandler(async (req: Request, res: Response) => {
+export const createLink = asyncHandler(async (req: ProtectedRequest, res: Response) => {
 	if (!req.body.longUrl) throw new BadRequestError("Long Url missing");
 	const linkRepo = getCustomRepository(LinkRepo);
 	const link = await linkRepo.create({
 		longUrl: req.body.longUrl,
-		shortUrl: crypto.randomBytes(3).toString("hex")
+		shortUrl: crypto.randomBytes(3).toString("hex"),
+		user: req.user
 	});
 	await linkRepo.save(link);
 	link.shortUrl = `${req.protocol}://${req.get("host")}/${link.shortUrl}`;
 	new SuccessResponse("success", link).send(res);
 });
 
-export const deleteLink = asyncHandler(async (req: Request, res: Response) => {
+export const deleteLink = asyncHandler(async (req: ProtectedRequest, res: Response) => {
 	const linkRepo = getCustomRepository(LinkRepo);
 	const link = await linkRepo.findByUuid(req.params.uuid);
 	if (!link) throw new NoEntryError("Link does not exist");
 	await linkRepo.delete({ id: link.id });
-	new SuccessMsgResponse("Successfully Deleted!");
+	new SuccessMsgResponse("Successfully Deleted!").send(res);
 });
--- src/routes/v1/link/link.ts
+++ src/routes/v1/link/link.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import authentication from "../../../auth/authentication";
 import { createLink, getLink, deleteLink } from "../../../controllers/v1/link/link";
 
 const router = express.Router();
+
+/* 
+  All the routes below this are private API
+  Protected by ACCESS_TOKEN
+*/
+router.use("/", authentication);
 
 router.post("/", createLink);
 router.get("/:uuid", getLink);
